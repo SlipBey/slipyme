0ee0d5acaa4141024692bda7f55b58da463eda2c
SlipBey <52493196+SlipBey@users.noreply.github.com>
2023-04-18 16:26:14 +0300

    Update projects.ts

--- libs/config/projects.ts
+++ libs/config/projects.ts
@@ -2,6 +2,7 @@ import enderrise from "@assets/projects/enderrise.svg";
 import code from "@assets/icon.svg";
 import design from "@assets/projects/design.svg";
 import deprem from "@assets/projects/slipymedeprem.svg";
+import todolist from "@assets/projects/todolist.svg";
 
 export const PROJECTS = [
   {
@@ -11,21 +12,27 @@ export const PROJECTS = [
     link: "https://code.slipyme.com",
   },
   {
+    icon: todolist,
+    title: "Slipyme Todolist",
+    text: "Slipyme'nin Yenilikçi Todolist Projesi!",
+    link: "https://todolist.slipyme.com",
+  },
+  {
     icon: deprem,
     title: "Slipyme Deprem",
-    text: "Depremde ne yapılması gerekir, yardımcı olacak kaynaklar neler? Her şeyi en düzenli şekilde listelediğimiz bir yardımcı kaynak sistemi.",
+    text: "Depremde ne yapılması gerekir, yardımcı olacak kaynaklar neler? Yardımcı bir kaynak sistemi..",
     link: "https://deprem.slipyme.com",
  },
   {
     icon: design,
     title: "Slipyme Design",
-    text: "Slipyme'nin Tasarımcılar için kurmuş olduğu tasarımcı platformudur. Platformda hayalinize göre tasarımlar yaptırabilir veya teknik yardımlar alabilirisniz.",
+    text: "Slipyme'nin Tasarımcılar için kurmuş olduğu tasarımcı platformudur. Platformda hayalinize göre tasarımlar yaptırabilir veya teknik yardımlar alabilirsiniz.",
     link: "https://design.slipyme.com",
   },
   {
     icon: enderrise,
     title: "EnderBot",
-    text: "EnderBot global bir çok amaçlı Disdcord botudur. işlerinizi kolaylaştırmaya yarar. Ve botu Slipyme ekibi geliştieip yönetmektedir.",
+    text: "EnderBot global bir çok amaçlı Disdcord botudur. işlerinizi kolaylaştırmaya yarar. Ve botu Slipyme ekibi geliştirip yönetmektedir.",
     link: "https://www.enderbot.net",
   },
   {
